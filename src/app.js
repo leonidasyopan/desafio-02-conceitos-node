e8d95541d5d9793d68abf1d729f97f25fc4e3fea
Leonidas Yopan <leonidasyopan@gmail.com>
2020-04-07 12:19:26 -0300

    desafio 02 completo. Sem erros.

--- src/app.js
+++ src/app.js
@@ -9,25 +9,31 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (request, response) => {  
+  // Return ALL the repositories from the list
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs, likes } = request.body;
+  // Store the input sent from the user into variables.
+  const { title, url, techs } = request.body;
+  // Create a unique ID using the uuid function.
   const id = uuid();
 
-  console.log(techs);
-
-  const project = { 
+  // Organize the array for the repository.
+  const repository = { 
     id: id,
     title: title,
     url: url,
     techs: techs,
-    likes: likes
+    likes: 0
   }
+
+  // Push the arry into the repositories list.
+  repositories.push(repository)
   
-  return response.json(project)
+  // Return the repository created to the user as JSON.
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -35,6 +41,7 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   // Stores the input date sent by the user into distinct varibles.
   const { title, url, techs } = request.body;
+  const likes = 0;
 
   // Creates a variable that represents the position of the array in which the desired
   // repository will be changed. It uses the function findIndex to compare the id.
@@ -50,7 +57,8 @@ app.put("/repositories/:id", (request, response) => {
     id,
     title,
     url,
-    techs
+    techs,
+    likes
   };
 
   // Places the new repository in the identified place the old one is located. This 
@@ -64,6 +72,26 @@ app.put("/repositories/:id", (request, response) => {
 
 app.delete("/repositories/:id", (req, res) => {
   // Stores the params (in this case the ID) passed through the URL into a variable.
+  const { id } = req.params;
+
+  // Creates a variable that represents the position of the array in which the desired
+  // repository will be changed. It uses the function findIndex to compare the id.
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  // Tests whether the ID was located. If it wasn't, returns an error message and status.
+  if(repositoryIndex < 0) {
+    return res.status(400).json({ error: "Repository Not Found"});
+  } 
+  
+  // Remove the repository's array from the object, using the index position.
+  repositories.splice(repositoryIndex, 1);
+
+  // Returns a success status to the user. No JSON message.
+  return res.status(204).send();
+
+});
+
+app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
   // Creates a variable that represents the position of the array in which the desired
@@ -73,18 +101,22 @@ app.delete("/repositories/:id", (req, res) => {
   // Tests whether the ID was located. If it wasn't, returns an error message and status.
   if(repositoryIndex < 0) {
     return response.status(400).json({ error: "Repository Not Found"});
-  } else {
-    // Remove the repository's array from the object, using the index position.
-    projects.splice(repositoryIndex, 1);
+  }
 
-    // Returns a success status to the user. No JSON message.
-    return response.status(204).send();
-  }  
+  // Store the number of likes before the update in a variable.
+  let initialLikes = repositories[repositoryIndex].likes;
+  console.log(initialLikes);
 
-});
+  // Update the number of likes by incrementing 1
+  let likes = initialLikes + 1;
+  console.log(likes);
 
-app.post("/repositories/:id/like", (request, response) => {
-  const { id } = request.params;
+  // Update the number of likes inside the proper repository ONLY.
+  repositories[repositoryIndex].likes = likes
+  console.log(repositories[repositoryIndex]);
+
+  // Returns a JSON with the number of current likes to the user.
+  return response.json({ "likes": likes });
 
 });
 
