ed3492879078727bd38abe9b74c603bc9dc7d613
Leonidas Yopan <leonidasyopan@gmail.com>
2020-04-06 22:37:39 -0300

    Project only started

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -15,7 +14,15 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const project = { 
+    id: uuid(), 
+    title: title, 
+    techs: techs, 
+    likes: 0 
+  }
+
 });
 
 app.put("/repositories/:id", (request, response) => {
